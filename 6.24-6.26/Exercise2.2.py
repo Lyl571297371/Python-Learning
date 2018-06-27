939cff2c744cfd425493cbfde7e1e182862e8c5d
Lyl571297371 <571297371@qq.com>
2018-06-27 09:00:01 +0800

    python learning 第一周

---
+++ 6.24-6.26/Exercise2.2.py
@@ -0,0 +1,6 @@
+#coding:gb2312
+#将一条消息存储在变量中，将其打印出来；再将变量的值修改为一条新信息，并将其打印出来
+language="c"
+print(language)
+language="python"
+print(language)
---
+++ 6.24-6.26/Exercise3.2.py
@@ -0,0 +1,9 @@
+#coding:gb2312
+#打印每个朋友的姓名，并为其打印一条问候信息。
+names=['cys','ljy','ft','hl','jy']
+message=","+"I'm very glad to meet you"+"!"
+print(names[0].title()+message)
+print(names[1].title()+message)
+print(names[2].title()+message)
+print(names[3].title()+message)
+print(names[4].title()+","+"I'm very glad to meet you"+"!")
