cbbccf7436be11f01c47b2c0080158716125f349
Lyl571297371 <571297371@qq.com>
2018-06-27 09:20:29 +0800

    del

--- 6.24-6.26/Exercise2.9.py
+++
@@ -1,5 +0,0 @@
-#coding:gb2312
-#将你最喜欢的一个数字存储在变量中，在创建一条消息并打印。
-numbers="962464"
-message="My Favorite Numbers Are:"+numbers+"!"
-print(message)
--- 6.24-6.26/Exercise3.1.py
+++
@@ -1,8 +0,0 @@
-#coding:gb2312
-#将一些朋友的姓名存储在列表中，依次访问每个元素，并将每个朋友的姓名打印出来。
-names=['cys','ljy','ft','hl','jy']
-print(names[0].title())
-print(names[1].title())
-print(names[-3].title())
-print(names[-2].title())
-print(names[-1].title())
