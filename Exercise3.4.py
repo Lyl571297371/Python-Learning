3109965f6f9cac64926dbbed96126fe515b06884
Lyl571297371 <39947669+Lyl571297371@users.noreply.github.com>
2018-06-27 08:54:37 +0800

    Delete Exercise3.4.py

--- Exercise3.4.py
+++
@@ -1,8 +0,0 @@
-#coding:gb2312
-#创建一个列表，打印信息，邀请列表的人一起进餐。
-Friends=['cby','sch','fjy','lq']
-message=", "+"Would You like To Have Dinner With Me"+"?"
-print(Friends[0].title()+message)
-print(Friends[1].title()+message)
-print(Friends[2].title()+message)
-print(Friends[-1].title()+", "+"Would You Like To Have Dinner With Me"+"?")
